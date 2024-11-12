2b548ffa54588b666629e5ccde5c4318a14d6527
nitinpahwa77 <nitinpahwa77@gmail.com>
2024-11-11 23:26:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_tenant2():
-    return "Welcome to Tenant 1's Application!"
+    return "Welcome to Tenant 2's Application!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
